0d7f1af3aaa676d6ad92dd75d0283763c355ae37
vjzed <68968881+vjzed@users.noreply.github.com>
2020-07-29 15:23:22 -0400

    my online git repo

---
+++ bmionline.js
@@ -0,0 +1,27 @@
+var mark = {
+		full_name: "London",
+		mass:200,
+		profession: "boxer",
+		height: 5.9,
+		bmiCalc: function(){
+				this.bmi = this.mass / (this.height * this.height);
+				return this.bmi;
+		}
+};
+
+var john = {
+		full_name: "JNewyork",
+		mass:200,
+		profession: ["wrestler", "Actor"],
+		height: 5.9,
+		bmiCalc: function(){
+				this.bmi = this.mass / (this.height * this.height);
+				return this.bmi;
+		}
+		
+};
+
+mark.bmiCalc();
+john.bmiCalc();
+
+console.log((mark.bmi > john.bmi)? "Mark's BMI is greater": ((john.bmi > mark.bmi)? "John's BMI is greater": "Both got same BMI" ));
